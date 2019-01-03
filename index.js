c8ddb626d65389877e6af4e3b0a77320be588148
Maldrek <scott.salem5@gmail.com>
2019-01-03 10:04:28 -0600

    header stylings finished

--- index.js
+++ index.js
@@ -1,17 +1,38 @@
-var array = [
-    {
-        q: 'what?',
-        a: ['huh?', 'who?', 'when', 'why?']
-    },
-    {
-        q: 'Who?',
-        a: 'why?'
-    }
-]
+// /* Write a function that takes an array of integers as an input and
+// then outputs the first duplicate */
 
-for(var i = 0; i < array.length; i++) {
-    console.log(array[i].q);
-    let question = array[i].q;
-    
-    $('.questionDiv').append(question);
+// let array = [1, 2, 3, 4, 5, 5, 3, 7, 8];
+
+// function hasDuplicate() {
+//     let array2 = [];
+//     for (let i = 0; i < array.length; i++) {
+//         for (var j = i+1; j < array.length; j++) {
+//             if (array[i] === array[j]) {
+//                 console.log('Duplicate found! The first duplicate number is ' + array[j]);
+//                 return;
+//             }
+//         }
+//     }
+//     console.log('There were no duplicates! Here is the array!');
+// }
+
+// hasDuplicate();
+
+
+var firstDup = function(arr){
+
+	var dupArr = [];
+
+	for (var i = 0; i < arr.length; i++) {
+		var val = arr[i];
+		if (dupArr[val]) {
+			console.log(val);
+		}
+		else{
+			dupArr[val] = true;
+		}
+	}
 }
+
+
+firstDup([1, 2, 3, 4, 2, 3, 4, 4]);
