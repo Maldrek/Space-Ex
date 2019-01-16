e50efc7f9e6cd1c81e3b6bfb127202aaf621fbbe
Maldrek <scott.salem5@gmail.com>
2019-01-16 17:16:24 -0600

    added package-lock.json

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-var isPangram = function (str) {
-	str = str.toLowerCase();
-	var alphabet = "abcdefghijklmnopqrstuvwxyz";
-	for (var i = 0; i < alphabet.length; i++) {
-		if (!str.includes(alphabet[i])){
-			return false;
-		}
-	}
-	return true;
-  }
+function drinkName(parameter) {
+	console.log(parameter);
+	//return random drink using user input
+};
 
-  console.log(isPangram('Five hexing wizard bots jump quickly'));
+
+// drinkName('gin');
+// drinkName('vodka');
+// drinkName('rum');
+
+var drinkType = 'gin';
+
+drinkName(drinkType);
