7627c109f3930e723c132997874074e1b7d889fc
Maldrek <scott.salem5@gmail.com>
2019-02-01 01:46:03 -0600

    removed js function

--- index.js
+++ index.js
@@ -1,12 +1,78 @@
-var isPangram = function (str) {
-	str = str.toLowerCase();
-	var alphabet = "abcdefghijklmnopqrstuvwxyz";
-	for (var i = 0; i < alphabet.length; i++) {
-		if (!str.includes(alphabet[i])){
-			return false;
-		}
-	}
-	return true;
-  }
 
-  console.log(isPangram('Five hexing wizard bots jump quickly'));
+
+
+// Seachbar Handler
+$(function () {
+	var searchField = $("#Searchq");
+	var icon = $("#search-BTN");
+	$('#search-BTN').on("click", function (e) {
+		e.preventDefault();
+		search();
+	});
+	//Focus event Handler
+	$(searchField).on('focus', function () {
+		$(this).animate({
+			width: '70%'
+		}, 400);
+		$(icon).animate({
+			right: '10px'
+		}, 400);
+	});
+	// Blur Even Handler   
+	$(searchField).on('blur', function () {
+		if (searchField.val() == '') {
+			$(searchField).animate({
+				width: '45%'
+			},
+				400);
+		}
+	})
+});
+function search() {
+	// clear results
+
+	$("#results").html('')
+	$("#buttons").html('')
+	// Get For Input
+	q = $("#Searchq").val().trim();
+	// Run Get Request
+	$.ajax("https://www.googleapis.com/youtube/v3/search?part=snippet&q=" + q + "&type=video&maxResults=3&key=AIzaSyBmBgUspUyuv5RHJj4nBnlOAC7F1Gn8w-M")
+		.then(function (response) {
+			console.log(response);
+			var VideoPush = $("#results")
+
+			for (i = 0; i < response.items.length; i++) {
+				var Pic = response.items[i].snippet.thumbnails.high.url;
+				var showPic = $("<img>").attr("src", Pic).addClass("VidResponse")
+				console.log(Pic);
+				VideoPush.append(showPic);
+
+				var Title = response.items[i].snippet.title;
+				var VidTitle = $("<H1>").text("Title: " + Title);
+				console.log(Title);
+				VideoPush.append(VidTitle);
+				var Description = response.items[i].snippet.description;
+				var VidDescription = $("<P>").text("Description: " + Description);
+				console.log(Description)
+				VideoPush.append(VidDescription);
+			};
+			var videoId1 = response.items[0].id;
+			console.log(videoId1);
+			// Load the IFrame Player API code asynchronously.
+			var tag = document.createElement('script');
+			tag.src = "https://www.youtube.com/player_api";
+			var firstScriptTag = document.getElementsByTagName('script')[0];
+			firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+			// Replace the 'ytplayer' element with an <iframe> and
+			// YouTube player after the API code downloads.
+			var player;
+			function onYouTubePlayerAPIReady() {
+				player = new YT.Player('ytplayer', {
+					height: '360',
+					width: '640',
+					videoId: videoId1,
+				});
+			}
+		});
+};
+
