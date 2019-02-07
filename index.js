f5d8e64c315d02d60e5a8b05336e9e57a3a03e11
Maldrek <scott.salem5@gmail.com>
2019-02-07 02:18:03 -0600

    added javascript functionality to navigation

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+$(document).ready(function () {
+
+    var checkbox = document.getElementById('nav-toggle');
+    console.log(checkbox);
+    console.log(checkbox.checked);
+
+    $('.navigation_link').on('click', function() {
+        console.log('a click!');
+        if (checkbox.checked === true) {
+            checkbox.checked = false;
+            console.log(checkbox.checked);
+        }
+    });
+})
 
